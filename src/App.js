10c74704e55b7241003c2041bb496fc1d7ba69d3
TimothyTerry <62906968+TimothyTerry@users.noreply.github.com>
2020-09-01 11:06:44 -0400

    readded

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Welcome from "./components/welcome/welcome";
+import Clock from "./components/Clock/clock";
+import Contact from "./components/Contact/contact";
+//Import the Navigation component
+import Navigation from "./components/navigation/navigation";
+import { Route,Switch } from "react-router-dom";
+import Errorpage from "./components/Errorpage/Error";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {/* render the Navigation component */}
+        <Navigation />
+        <Switch >
+        <Route
+          exact
+          path="/"
+          render={(props) => <Welcome {...props} name="eric" />}
+         
+        />
+
+        <Route
+          
+          path="/welcome/:name"
+          render={(props) => <Welcome {...props} name={props.match.params.name} />}
+        />
+        <Route path="/clock" component={Clock} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/Errorpage" component={Errorpage} />
+         </Switch>
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/components/Errorpage/Error.js
@@ -0,0 +1,15 @@
+//import React from './node_modules/react'
+import React from "react"
+
+
+function Errorpage (props) {
+    return (
+        <div>Page Error</div>
+    );
+
+}
+
+
+
+
+export default Errorpage;
